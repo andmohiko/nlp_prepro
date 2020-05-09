f3d2a2c0890bfe0564e69d4cf7e96f8f6155fde0
andmohiko <thomasito.berryz@gmail.com>
2020-05-09 14:13:44 +0900

    品詞・活用形を考慮

--- README.md
+++ README.md
@@ -17,7 +17,23 @@ pip install -r requirements.txt
 ### Word split
 
 ```python
-df["word list"] = split_words(make_corpus(df["sentence"]))
+t = "今日は曇りのち晴れです。走りたい気分です。全角！半角!"
+
+# 全品詞
+print(get_words(t))
+# ['今日', 'は', '曇り', 'のち', '晴れ', 'です', '。', '走り', 'たい', '気分', 'です', '。', '全角', '！', '半角', '!']
+
+# 任意の品詞のみ
+print(get_words(t, pos_list=["名詞", "動詞"]))
+# ['今日', '曇り', 'のち', '晴れ', '走り', '気分', '全角', '半角', '!']
+
+# 原形
+print(get_words(t, pos_list=["名詞", "動詞"], form="origin"))
+# ['今日', '曇る', 'のち', '晴れ', '走る', '気分', '全角', '半角', '!']
 ```
+
+<!-- ```python
+df["word list"] = split_words(make_corpus(df["sentence"]))
+``` -->
 
 ### Make dictionary
--- wordsplit.py
+++ wordsplit.py
@@ -1,11 +1,45 @@
+import re
+
 import numpy as np
 import MeCab
 
 
-wakati = MeCab.Tagger('-Owakati')
+nmc = MeCab.Tagger('-Ochasen')
+nmw = MeCab.Tagger("-Owakati")
+
 
 def get_wakati(text):
-    return wakati.parse(text).strip()
+    return nmw.parse(text).strip()
+
+
+def get_words(text, pos_list=[], form="asitis"):
+    word_list = []
+    inflection_index = inflection(form)
+    for chunk in nmc.parse(text).splitlines()[:-1]:
+        morpheme = chunk.split('\t')
+        morpheme = parts_of_speech(morpheme, pos_list)
+        if morpheme == None:
+            continue
+        word_list.append(morpheme[inflection_index])
+    return word_list
+
+
+def parts_of_speech(morpheme, pos_list):
+    if pos_list == []:
+        return morpheme
+    else:
+        for pos in pos_list:
+            if morpheme[3].startswith(pos):
+                return morpheme
+
+
+def inflection(form):
+    if form == "asitis":
+        return 0
+    elif form == "origin":
+        return 2
+    else:
+        return "error in form"
 
 
 def make_corpus(series):
@@ -40,11 +74,3 @@ def remove_stopwords(corpus_splitted, stopwords):
         if corpus_splitted[i] == []:
             na_idx.append(i)
     return corpus_splitted, na_idx
-
-
-def prepro(data, stopwords):
-    corpus_splitted = split_words(make_corpus(data["sentence"]))
-    corpus_rmsw, na_idx = remove_stopwords(corpus_splitted, stopwords)
-    data["wordlist"] = corpus_rmsw
-    data = data.drop(na_idx).reset_index(drop=True)
-    return data
